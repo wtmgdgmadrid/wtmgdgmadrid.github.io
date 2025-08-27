92a7de7814ed0672757fa2ab19a9b051c987eb02
nujovich <nujovich@gmail.com>
2025-08-27 16:58:32 +0200

    fix: update site URL and base path in astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -22,6 +22,8 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
   hasExternalScripts ? (Array.isArray(items) ? items.map((item) => item()) : [items()]) : [];
 
 export default defineConfig({
+  site: 'https://wtmgdmadrid.github.io/wtm/',
+  base: '/wtm-madrid-landing/',
   output: 'static',
 
   integrations: [
