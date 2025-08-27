aa4cfdcd9b9aa9dfb66192d6d3086d6e30c317fc
nujovich <nujovich@gmail.com>
2025-08-27 17:16:57 +0200

    fix: update site URL in astro.config.ts to reflect correct landing page

--- astro.config.ts
+++ astro.config.ts
@@ -22,7 +22,7 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
   hasExternalScripts ? (Array.isArray(items) ? items.map((item) => item()) : [items()]) : [];
 
 export default defineConfig({
-  site: 'https://wtmgdmadrid.github.io/wtm/',
+  site: 'https://wtmgdmadrid.github.io/wtm-madrid-landing/',
   base: '/wtm-madrid-landing/',
   output: 'static',
 
